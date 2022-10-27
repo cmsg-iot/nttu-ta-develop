815aba46e8b4f02f9109637de0a5c3567262e3ec
ceyang <ceyanglab@gmail.com>
2022-10-27 17:50:51 +0800

    refact function

--- Raspberry-Pi-Pico/projects/gas-detected-system/controller/commandHandler.py
+++ Raspberry-Pi-Pico/projects/gas-detected-system/controller/commandHandler.py
@@ -7,30 +7,30 @@ class CommandHandler:
         self.cmd_queue = []
         self.allow_cmd_list = allow_cmd_list
     
-    # 檢查命令是否在名單中
-    def checkCommand(self,cmd):
+    # 檢查命令是否在允許的名單中
+    def checkCommandInAllowedList(self,cmd):
         for i in self.allow_cmd_list:
             if i in cmd:
                 return True
         return False
 
-    # 格式化從uart過來的指令
+    # 格式化從uart過來的命令
     def formatCommand(self,cmd):
         if len(cmd) == 0: return
         str = cmd.decode("utf-8")
         if ("vm" in str) and ("\r\n" in str):
             return str.split("<")[1].split("\r\n")[0]
+        return "Not vaild command"
     
-    # 將允許的指令加入 queue 中
-    def addAllowedCommand(self,cmd):
-        c = self.formatCommand(cmd)
-        if c != None and self.checkCommand(c):
-            self.cmd_queue.append(c)
+    # 將命令加入 queue 中
+    def addCommandToQueue(self,cmd):
+        self.cmd_queue.append(cmd)
     
     # 先進先出執行 queue 中的命令
     def executeCommandFromQueue(self):
         if len(self.cmd_queue) > 0:
             cmd = self.cmd_queue.pop(0)
+            model.store.excuteCommand(cmd)
             message = "excuted: " + cmd
             print(message)
-            model.store.setMessage(message)
+            

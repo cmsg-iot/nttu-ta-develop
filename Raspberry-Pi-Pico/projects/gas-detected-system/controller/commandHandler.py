615d65b56837b6ed10dbc0e59484dbd9b2136d9a
ceyang <ceyanglab@gmail.com>
2022-10-28 15:29:37 +0800

    tidy code

--- Raspberry-Pi-Pico/projects/gas-detected-system/controller/commandHandler.py
+++ Raspberry-Pi-Pico/projects/gas-detected-system/controller/commandHandler.py
@@ -1,33 +1,31 @@
-import model.store
 import sys
 sys.path.append("../model")
-
+import model.store
 
 class CommandHandler:
     def __init__(self, allow_cmd_list=[]):
         self.cmd_queue = []
         self.allow_cmd_list = allow_cmd_list
-
+    
     # 檢查命令是否在允許的名單中
-    def checkCommandInAllowedList(self, cmd):
+    def checkCommandInAllowedList(self,cmd):
         for i in self.allow_cmd_list:
             if i in cmd:
                 return True
         return False
 
     # 格式化從uart過來的命令
-    def formatCommand(self, cmd):
-        if len(cmd) == 0:
-            return
+    def formatCommand(self,cmd):
+        if len(cmd) == 0: return
         if ("vm<" in cmd):
             s = str(cmd)[5:-5]
             return s
         return "Not vaild command"
-
+    
     # 將命令加入 queue 中
-    def addCommandToQueue(self, cmd):
+    def addCommandToQueue(self,cmd):
         self.cmd_queue.append(cmd)
-
+    
     # 先進先出執行 queue 中的命令
     def executeCommandFromQueue(self):
         if len(self.cmd_queue) > 0:
--- Raspberry-Pi-Pico/projects/gas-detected-system/model/store.py
+++ Raspberry-Pi-Pico/projects/gas-detected-system/model/store.py
@@ -4,7 +4,7 @@ from modules.HX711 import HX711
 from modules.ADC import Adc
 from modules.UART import Uart
 import parser.configParser
-from machine import Pin, UART
+from machine import Pin,UART
 import uasyncio as asyncio
 from utime import sleep_ms
 import json
@@ -38,25 +38,22 @@ allow_cmd_list = [
 ]
 
 configPath = "/config/config.json"
-configTargets = ["pressure_in", "pressure_out", "mq2", "mq2_2", "mq7", "hx711"]
+configTargets = ["pressure_in","pressure_out","mq2","mq2_2","mq7","hx711"]
 
-am2320 = AM2320(i2cNum=0, scl=17, sda=16, freq=100000)
-hx711 = HX711(d_out=4, pd_sck=5)
-hall = GPIO(pin=10, mode=0)
-buzzer = GPIO(pin=11, mode=1)
-heater = GPIO(pin=12, mode=1)
-valveOpen = GPIO(pin=13, mode=1)
-valveClose = GPIO(pin=14, mode=1)
+am2320 = AM2320(i2cNum=0,scl=17,sda=16,freq=100000)
+hx711 = HX711(d_out=4,pd_sck=5)
+hall = GPIO(pin=10,mode=0)
+buzzer = GPIO(pin=11,mode=1)
+heater = GPIO(pin=12,mode=1)
+valveOpen = GPIO(pin=13,mode=1)
+valveClose = GPIO(pin=14,mode=1)
 mq7 = Adc(channel=0)
 mq2 = Adc(channel=1)
 mq2_2 = Adc(channel=2)
 pressure_in = Adc(channel=3)
 pressure_out = Adc(channel=4)
-#uart=Uart(0, baudrate=115200, parity=None, stop=1, bits=8,timeout=3)
-uart = UART(0, baudrate=115200, parity=None, stop=1, bits=8)
-# uart.init()
+uart=UART(0, baudrate=115200, parity=None, stop=1, bits=8)
 commandHandler = CommandHandler(allow_cmd_list=allow_cmd_list)
-
 
 def initialDeviceStateWithConfigFile(state):
     for target in state:
@@ -70,51 +67,44 @@ def initialDeviceStateWithConfigFile(state):
             elif param == "ch":
                 t.setChannel(v)
 
-
 def excuteCommand(cmd):
-    # Pin(25,Pin.OUT).value(1)
-    print(cmd)
+    Pin(25,Pin.OUT).value(1)
     message = "excuted: " + cmd
     setMessage(message)
     logMessage(message)
     cmd_list = getSeperatedCommandList(cmd)
     dispatchAction(cmd_list)
     sleep_ms(10)
-    # Pin(25,Pin.OUT).value(0)
-    writeTargetsParamToFile(configPath, configTargets)
-
+    Pin(25,Pin.OUT).value(0)
+    writeTargetsParamToFile(configPath,configTargets)
 
 def getSeperatedCommandList(cmd):
     cmd_list = separateCommandWithSpace(cmd)
     cmd_list = formatCommandList(cmd_list)
     return cmd_list
 
-
 def separateCommandWithSpace(cmd):
     return cmd.split()
 
-
 def formatCommandList(cmd_list):
-    new_list = ["", "", "", None]
-    for idx, v in enumerate(cmd_list):
+    new_list = ["","","",None]
+    for idx,v in enumerate(cmd_list):
         new_list[idx] = v
     return new_list
 
-
 def dispatchAction(cmd_list):
     if cmd_list[0] == "set" and cmd_list[3] != None:
-        setTargetWithValue(cmd_list[1], cmd_list[2], cmd_list[3])
+        setTargetWithValue(cmd_list[1],cmd_list[2],cmd_list[3])
     elif cmd_list[0] == "set" and cmd_list[3] == None:
-        setTarget(cmd_list[1], cmd_list[2])
+        setTarget(cmd_list[1],cmd_list[2])
     elif cmd_list[0] == "reset":
-        resetTarget(cmd_list[1], cmd_list[2])
+        resetTarget(cmd_list[1],cmd_list[2])
     elif cmd_list[0] == "log":
         setLog(cmd_list[1])
     else:
-        setMessage("not vaild method")
+        setMessage("Not vaild method")
 
-
-def setTargetWithValue(target, act, value):
+def setTargetWithValue(target,act,value):
     if act == "on":
         getTargetObject(target).openWithTime(value)
     elif act == "shift":
@@ -124,10 +114,9 @@ def setTargetWithValue(target, act, value):
     elif act == "ch":
         getTargetObject(target).setChannel(value)
     else:
-        setMessage("not vaild target")
+        setMessage("Not vaild target")
 
-
-def setTarget(target, act):
+def setTarget(target,act):
     if act == "on":
         getTargetObject(target).setValue(1)
     elif act == "off":
@@ -135,15 +124,14 @@ def setTarget(target, act):
     elif act == "zero":
         getTargetObject(target).setShiftZero()
     else:
-        setMessage("missing 1 required argument")
+        setMessage("Missing 1 required argument")
+    
 
-
-def resetTarget(target, act):
-    if act == "zero":
+def resetTarget(target,act):
+    if act =="zero":
         getTargetObject(target).resetShiftZero()
     else:
-        setMessage("not vaild target")
-
+        setMessage("Not vaild target")
 
 def getTargetObject(target=""):
     if target == "pressure_in":
@@ -169,11 +157,9 @@ def getTargetObject(target=""):
     else:
         return None
 
-
-def writeTargetsParamToFile(path, targets):
+def writeTargetsParamToFile(path,targets):
     jsonString = formatTartgetsParamToJson(targets)
-    writeJsonToFile(path, jsonString)
-
+    writeJsonToFile(path,jsonString)
 
 def formatTartgetsParamToJson(targets):
     j = {}
@@ -185,17 +171,11 @@ def formatTartgetsParamToJson(targets):
         j[i]['radial'] = getTargetObject(i).radial
     return json.dumps(j)
 
-
-def writeJsonToFile(path, jsonString):
-    # Pin(25,Pin.OUT).value(1)
-    f = open(path, 'w')
+def writeJsonToFile(path,jsonString):
+    f = open(path,'w')
     f.write(jsonString)
-    # f.flush()
     f.close()
     logMessage("Write json to: " + path)
-    sleep_ms(1000)
-    # Pin(25,Pin.OUT).value(0)
-
 
 def setLog(flag):
     global __state
@@ -204,50 +184,46 @@ def setLog(flag):
     else:
         __state['log'] = False
 
-
 def logMessage(message):
     if __state['log']:
         print(message)
-
 
 def setMessage(message):
     global __state
     __state['message'] = message
 
-
 global __state
 __state = {
-    'data': {
-        'pressure_in': 0.0,
-        'pressure_out': 0.0,
-        'temp': 0.0,
-        'hum': 0.0,
-        'hx711': 0.0,
-        'mq2': 0,
-        'mq2_2': 0,
-        'mq7': 0,
-        'hall': 0,
-        'buzzer': 0,
-        'heater': 0,
-        'valveOpen': 0,
-        'valveClose': 0
+    'data':{
+        'pressure_in':0.0,
+        'pressure_out':0.0,
+        'temp':0.0,
+        'hum':0.0,
+        'hx711':0.0,
+        'mq2':0,
+        'mq2_2':0,
+        'mq7':0,
+        'hall':0,
+        'buzzer':0,
+        'heater':0,
+        'valveOpen':0,
+        'valveClose':0
     },
-    'uart': None,
-    'message': "",
-    'log': False
+    'uart':None,
+    'message':"",
+    'log':False
 }
 
 initialDeviceStateWithConfigFile(parser.configParser.__deviceConfig)
-
 
 async def readData():
     global __state
     swriter = asyncio.StreamWriter(uart, {})
     while True:
-
+        
         __state['data']['pressure_in'] = pressure_in.getValue()
         await asyncio.sleep_ms(5)
-
+        
         __state['data']['pressure_out'] = pressure_out.getValue()
         await asyncio.sleep_ms(5)
 
@@ -256,7 +232,7 @@ async def readData():
             __state['data']['temp'] = am2320Data['temp']
             __state['data']['hum'] = am2320Data['hum']
         await asyncio.sleep_ms(5)
-
+    
         __state['data']['hx711'] = hx711.getValue()
         await asyncio.sleep_ms(5)
 
@@ -278,17 +254,17 @@ async def readData():
         __state['data']['heater'] = heater.getValue()
         await asyncio.sleep_ms(5)
 
-        __state['data']['valveOpen'] = valveOpen.getValue()
+        __state['data']['valveOpen'] =valveOpen.getValue()
         await asyncio.sleep_ms(5)
 
-        __state['data']['valveClose'] = valveClose.getValue()
+        __state['data']['valveClose'] =valveClose.getValue()
         await asyncio.sleep_ms(5)
-
+        
         newData = dict(__state['data'])
-        message = {'message': __state['message']}
+        message = {'message':__state['message']}
         newData.update(message)
         j = json.dumps(newData)
-        # print(j)
+
         # clear message
         if len(__state['message']) > 0:
             __state['message'] = ""
@@ -296,46 +272,39 @@ async def readData():
         await swriter.drain()
         await asyncio.sleep(1)
 
-
 async def receiver():
     sreader = asyncio.StreamReader(uart)
     while True:
         res = await sreader.readline()
 
-
 async def readUART():
     global __state
-    swriter = asyncio.StreamWriter(uart, {})
-    sreader = asyncio.StreamReader(uart)
+    sreader = asyncio.StreamReader(uart, {})
     while True:
         cmd = await sreader.readline()
-
-        if cmd != None and cmd != b'':
+        
+        if cmd != None and cmd != b'' :
             cmd = commandHandler.formatCommand(cmd)
-            # Pin(25,Pin.OUT).value(1)
-            if not commandHandler.checkCommandInAllowedList(cmd):
-                # print(cmd)
+            if commandHandler.checkCommandInAllowedList(cmd):
                 if cmd == "help":
                     l = {'allow_cmd_list': allow_cmd_list}
-                    await swriter.write(str(json.dumps(l)))
-            else:
+                    l = json.dumps(l)
+                    print(l)
+                    uart.write(l)
                 commandHandler.addCommandToQueue(cmd)
 
         commandHandler.executeCommandFromQueue()
         logMessage(cmd)
         cmd = None
         await sreader.drain()
-        await swriter.drain()
-        await asyncio.sleep_ms(100)
-
+        await asyncio.sleep_ms(250)
 
 async def main():
     asyncio.create_task(readData())
     asyncio.create_task(readUART())
-
+    
     while True:
         await asyncio.sleep(1)
-
 
 def start():
     try:
@@ -345,6 +314,5 @@ def start():
     finally:
         asyncio.new_event_loop()
         print('as_demos.auart.test() to run again.')
-
 
 start()

d21938160036e679f4bd50921e87e2478fd09b71
ceyang <ceyanglab@gmail.com>
2022-11-01 10:05:02 +0800

    update: refact store and state, allow command list

--- Raspberry-Pi-Pico/projects/gas-detected-system/model/store.py
+++ Raspberry-Pi-Pico/projects/gas-detected-system/model/store.py
@@ -2,42 +2,18 @@ from modules.AM2320 import AM2320
 from modules.GPIO import GPIO
 from modules.HX711 import HX711
 from modules.ADC import Adc
-from modules.UART import Uart
-import parser.configParser
 from machine import Pin,UART
 import uasyncio as asyncio
 from utime import sleep_ms
 import json
-import os
-from controller.commandHandler import CommandHandler
-
-allow_cmd_list = [
-    "set pressure_in shift",
-    "set pressure_in radial",
-    "set pressure_out shift",
-    "set pressure_out radial",
-    "set mq2 shift",
-    "set mq2 radial",
-    "set mq7 shift",
-    "set mq7 radial",
-    "set hx711 ch",
-    "set hx711 shift",
-    "set hx711 radial",
-    "set buzzer on",
-    "set buzzer off",
-    "set heater on",
-    "set heater off",
-    "set valveOpen on",
-    "set valveOpen off",
-    "set valveClose on",
-    "set valveClose off",
-    "log on",
-    "log off",
-    "help"
-]
+from utility.commandList import getSeperatedCommandList
+from utility.configParser import getConfigFromPath
+from utility.file import updateFileWithState
+from utility.common import logMessage
+import state
 
 configPath = "/config/config.json"
-configTargets = ["pressure_in","pressure_out","mq2","mq2_2","mq7","hx711"]
+#configTargets = ["pressure_in","pressure_out","mq2","mq2_2","mq7","hx711"]
 
 am2320 = AM2320(i2cNum=0,scl=17,sda=16,freq=100000)
 hx711 = HX711(d_out=4,pd_sck=5)
@@ -52,44 +28,21 @@ mq2_2 = Adc(channel=2)
 pressure_in = Adc(channel=3)
 pressure_out = Adc(channel=4)
 uart=UART(0, baudrate=115200, parity=None, stop=1, bits=8)
-commandHandler = CommandHandler(allow_cmd_list=allow_cmd_list)
 
-def initialDeviceStateWithConfigFile(state):
-    for target in state:
-        t = getTargetObject(target)
-        for param in state[target]:
-            v = state[target][param]
-            if param == "shift":
-                t.setShift(v)
-            elif param == "radial":
-                t.setRadial(v)
-            elif param == "ch":
-                t.setChannel(v)
+global __state
+__state = state.state
 
 def excuteCommand(cmd):
+    global __state
     Pin(25,Pin.OUT).value(1)
-    message = "excuted: " + cmd
+    message = "Excuted: " + cmd
     setMessage(message)
-    logMessage(message)
+    logMessage(message,__state['log'])
     cmd_list = getSeperatedCommandList(cmd)
     dispatchAction(cmd_list)
-    sleep_ms(10)
+    sleep_ms(50)
     Pin(25,Pin.OUT).value(0)
-    writeTargetsParamToFile(configPath,configTargets)
-
-def getSeperatedCommandList(cmd):
-    cmd_list = separateCommandWithSpace(cmd)
-    cmd_list = formatCommandList(cmd_list)
-    return cmd_list
-
-def separateCommandWithSpace(cmd):
-    return cmd.split()
-
-def formatCommandList(cmd_list):
-    new_list = ["","","",None]
-    for idx,v in enumerate(cmd_list):
-        new_list[idx] = v
-    return new_list
+    updateFileWithState(configPath,__state)
 
 def dispatchAction(cmd_list):
     if cmd_list[0] == "set" and cmd_list[3] != None:
@@ -99,9 +52,10 @@ def dispatchAction(cmd_list):
     elif cmd_list[0] == "log":
         setLog(cmd_list[1])
     else:
-        setMessage("Not vaild method")
+        setMessage("Not vaild method: " + cmd_list[0])
 
 def setTargetWithValue(target,act,value):
+    global __state
     if act == "on":
         getTargetObject(target).openWithTime(value)
     elif act == "shift":
@@ -112,6 +66,9 @@ def setTargetWithValue(target,act,value):
         getTargetObject(target).setChannel(value)
     else:
         setMessage("Not vaild target")
+    
+    if act != "on":
+        __state['isConfigUpdate'] = True
 
 def setTarget(target,act):
     if act == "on":
@@ -147,25 +104,19 @@ def getTargetObject(target=""):
     else:
         return None
 
-def writeTargetsParamToFile(path,targets):
-    jsonString = formatTartgetsParamToJson(targets)
-    writeJsonToFile(path,jsonString)
+def initialDeviceStateWithConfigFile(state):
+    for target in state:
+        t = getTargetObject(target)
+        for param in state[target]:
+            v = state[target][param]
+            if param == "shift":
+                t.setShift(v)
+            elif param == "radial":
+                t.setRadial(v)
+            elif param == "ch":
+                t.setChannel(v)
 
-def formatTartgetsParamToJson(targets):
-    j = {}
-    for i in targets:
-        j[i] = {}
-        if i == "hx711":
-            j[i]['ch'] = getTargetObject(i).channel
-        j[i]['shift'] = getTargetObject(i).shift
-        j[i]['radial'] = getTargetObject(i).radial
-    return json.dumps(j)
-
-def writeJsonToFile(path,jsonString):
-    f = open(path,'w')
-    f.write(jsonString)
-    f.close()
-    logMessage("Write json to: " + path)
+initialDeviceStateWithConfigFile(getConfigFromPath("/config/config.json"))
 
 def setLog(flag):
     global __state
@@ -174,65 +125,9 @@ def setLog(flag):
     else:
         __state['log'] = False
 
-def logMessage(message):
-    if __state['log']:
-        print(message)
-
 def setMessage(message):
     global __state
     __state['message'] = message
-
-global __state
-__state = {
-    'data':{
-        'pressure_in':0.0,
-        'pressure_out':0.0,
-        'temp':0.0,
-        'hum':0.0,
-        'hx711':0.0,
-        'mq2':0,
-        'mq2_2':0,
-        'mq7':0,
-        'hall':0,
-        'buzzer':0,
-        'heater':0,
-        'valveOpen':0,
-        'valveClose':0
-    },
-    'config':{
-        'pressure_in':{
-            'shift':0,
-            'radial':1.0
-        },
-        'pressure_out':{
-            'shift':0,
-            'radial':1.0
-        },
-        'mq2':{
-            'shift':0,
-            'radial':1.0
-        },
-        'mq2_2':{
-            'shift':0,
-            'radial':1.0
-        },
-        'mq7':{
-            'shift':0,
-            'radial':1.0
-        },
-        'hx711':{
-            'ch':0,
-            'shift':0,
-            'radial':1.0
-        }
-    },
-    'uart':None,
-    'message':"",
-    'log':False
-}
-
-
-initialDeviceStateWithConfigFile(parser.configParser.__deviceConfig)
 
 async def readData():
     global __state
@@ -298,25 +193,25 @@ async def readData():
         __state['config']['mq2']['radial'] = mq2.radial
         await asyncio.sleep_ms(5)
 
-        __state['config']['shift'] = mq2_2.shift
+        __state['config']['mq2_2']['shift'] = mq2_2.shift
         await asyncio.sleep_ms(5)
 
-        __state['config']['radial'] = mq2_2.radial
+        __state['config']['mq2_2']['radial'] = mq2_2.radial
         await asyncio.sleep_ms(5)
 
-        __state['config']['shift'] = mq7.shift
+        __state['config']['mq7']['shift'] = mq7.shift
         await asyncio.sleep_ms(5)
 
-        __state['config']['radial'] = mq7.radial
+        __state['config']['mq7']['radial'] = mq7.radial
         await asyncio.sleep_ms(5)
 
-        __state['config']['shift'] = hx711.shift
+        __state['config']['hx711']['shift'] = hx711.shift
         await asyncio.sleep_ms(5)
 
-        __state['config']['radial'] = hx711.radial
+        __state['config']['hx711']['radial'] = hx711.radial
         await asyncio.sleep_ms(5)
 
-        __state['config']['ch'] = hx711.channel
+        __state['config']['hx711']['ch'] = hx711.channel
         await asyncio.sleep_ms(5)
         
         newData = {}
@@ -335,41 +230,18 @@ async def readData():
         await swriter.drain()
         await asyncio.sleep(1)
 
-async def receiver():
-    sreader = asyncio.StreamReader(uart)
-    while True:
-        res = await sreader.readline()
-
 async def readUART():
     global __state
     sreader = asyncio.StreamReader(uart, {})
     while True:
         cmd = await sreader.readline()
-        
-        if cmd != None and cmd != b'' :
-            cmd = commandHandler.formatCommand(cmd)
-            if commandHandler.checkCommandInAllowedList(cmd):
-                if cmd == "help":
-                    l = {'allow_cmd_list': allow_cmd_list}
-                    l = json.dumps(l)
-                    print(l)
-                    uart.write(l)
-                commandHandler.addCommandToQueue(cmd)
-
-        commandHandler.executeCommandFromQueue()
-        logMessage(cmd)
-        cmd = None
+        __state['uart'] = cmd
+        logMessage(cmd,__state['log'])
+        cmd = ""
         await sreader.drain()
         await asyncio.sleep_ms(250)
 
-def getFileListOfPath(rootPath):
-    file_list = []
-    for i in os.listdir(rootPath):
-        file_list.append(rootPath + "/" + i)
-    return file_list
-
 async def executeRules(path_list):
-    print(path_list)
     while True:
         for i in path_list:
             f = open(i,'r')
@@ -377,24 +249,3 @@ async def executeRules(path_list):
             f.close()
             await asyncio.sleep_ms(50)
         await asyncio.sleep(1)
-
-async def main():
-    path_list = getFileListOfPath('/rules')
-    asyncio.create_task(readData())
-    asyncio.create_task(readUART())
-    #asyncio.create_task(executeRules(path_list))
-    
-    while True:
-        await asyncio.sleep(1)
-
-def start():
-    try:
-        asyncio.run(main())
-    except OSError:
-        print("error")
-    finally:
-        asyncio.new_event_loop()
-        print('as_demos.auart.test() to run again.')
-
-start()
-
